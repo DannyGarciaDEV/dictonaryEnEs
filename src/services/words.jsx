f35560b6ece0a9d78debf620753525ba324f904a
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-01-19 11:21:37 -0500

    fixing axios to fetch from the traslator api

--- src/services/words.jsx
+++ src/services/words.jsx
@@ -1,36 +1,49 @@
 import axios from 'axios';
+
 const baseUrl = 'http://localhost:3001/words';
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then(response => {
+const getAll = async () => {
+  try {
+    const response = await axios.get(baseUrl);
     console.log('Get All Words Response:', response.data);
     return response.data;
-  });
+  } catch (error) {
+    console.error('Get All Words Error:', error);
+    throw error; // Propagate the error to the caller
+  }
 };
 
-const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then(response => {
+const create = async (newObject) => {
+  try {
+    const response = await axios.post(baseUrl, newObject);
     console.log('Create Word Response:', response.data);
     return response.data;
-  });
+  } catch (error) {
+    console.error('Create Word Error:', error);
+    throw error;
+  }
 };
 
-const removeWord = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then(response => {
+const removeWord = async (id) => {
+  try {
+    const response = await axios.delete(`${baseUrl}/${id}`);
     console.log('Remove Word Response:', response.data);
     return response.data;
-  });
+  } catch (error) {
+    console.error('Remove Word Error:', error);
+    throw error;
+  }
 };
 
-const updateWord = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then(response => {
+const updateWord = async (id, newObject) => {
+  try {
+    const response = await axios.put(`${baseUrl}/${id}`, newObject);
     console.log('Update Word Response:', response.data);
     return response.data;
-  });
+  } catch (error) {
+    console.error('Update Word Error:', error);
+    throw error;
+  }
 };
 
 export default { getAll, create, removeWord, updateWord };
