e931c6783736ddb843280dfddfffd46b5a062b7e
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-01-22 18:01:51 -0500

    adding css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Navbar.css'; // Import your CSS file for styling
 import { Link } from 'react-router-dom';
+
+
 const Navbar = () => {
   return (
     <div className="navbar">
-    <input type="checkbox" id="menu-toggle" className="menu-toggle" />
-    <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
-    <ul className="nav-links">
-      <li><Link to="/App">Home</Link></li>
-      <li><Link to="/FlashCards">FlashCards</Link></li>
-      
-    </ul>
-  </div>
-);
+      <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+      <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
+      <ul className="nav-links">
+        <li><Link to="/App">Home</Link></li>
+        <li><Link to="/FlashCards">FlashCards</Link></li>
+       
+      </ul>
+    </div>
+  );
 };
+
 export default Navbar;
