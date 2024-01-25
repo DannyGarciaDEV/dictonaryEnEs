43d273f1a7b3c1ad1d9936bec82fbc91f9bd6cd7
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-01-24 19:34:54 -0500

    keeping nav bar and removing links is not working the modules

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,13 +6,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <input type="checkbox" id="menu-toggle" className="menu-toggle" />
-      <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
-      <ul className="nav-links">
-        <li><Link to="/App">Home</Link></li>
-        <li><Link to="/FlashCards">FlashCards</Link></li>
-       
-      </ul>
+
     </div>
   );
 };
